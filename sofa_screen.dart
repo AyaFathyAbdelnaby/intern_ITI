d184ad7cbb13947a6a29aec0e357f4141cd9d35a
AyaFathyAbdelnaby <139823268+AyaFathyAbdelnaby@users.noreply.github.com>
2023-08-01 23:18:55 +0300

    Update sofa_screen.dart

--- sofa_screen.dart
+++ sofa_screen.dart
@@ -5,9 +5,51 @@ class SofaScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [Icon(Icons.arrow_back), Icon(Icons.favorite)],
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 26),
+          child: Column(
+            children: [
+              const SizedBox(height: 36),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.arrow_back),
+                  Icon(Icons.favorite_border),
+                ],
+              ),
+              Image.asset(
+                "assets/sofa.png",
+                width: 278,
+                height: 303,
+              ),
+              const SizedBox(height: 21),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Rome Sofa",
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                  ),
+                  Container(
+                    width: 93,
+                    height: 30,
+                    decoration: const BoxDecoration(color: Color(0xffE7E7E7)),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Icon(Icons.remove),
+                        Text("1"),
+                        Icon(Icons.add),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
